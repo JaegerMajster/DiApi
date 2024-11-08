75b673561561763c32dd17cfba6672b043c58e78
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-11-08 14:28:37 +0100

    Adding Dependency injection in class

--- Data/NoSqlDataRepo.cs
+++ Data/NoSqlDataRepo.cs
@@ -1,11 +1,20 @@
+using DiApi.DataServices;
+
 namespace DiApi.Data
 {
     public class NoSqlDataRepo : IDataRepo
     {
+        private readonly IDataService _dataService;
+
+        public NoSqlDataRepo(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
         public string ReturnData()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("--> Getting data from No SQL Databse...");
+            _dataService.GetProductData("https://ckziumm.edu.pl");
             Console.ResetColor();
 
             return("No SQL Data from DB");
