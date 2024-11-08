9e1630ff642669e96a861f48ccb1ba756bb9a17f
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-11-08 15:07:11 +0100

    Walkaround to using Singleton in specific scope

--- Data/NoSqlDataRepo.cs
+++ Data/NoSqlDataRepo.cs
@@ -4,20 +4,33 @@ namespace DiApi.Data
 {
     public class NoSqlDataRepo : IDataRepo
     {
-        private readonly IDataService _dataService;
 
-        public NoSqlDataRepo(IDataService dataService)
+        // private readonly IDataService _dataService;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        // public NoSqlDataRepo(IDataService dataService)
+        // {
+        //     _dataService = dataService;
+        // }
+
+        public NoSqlDataRepo(IServiceScopeFactory scopeFactory)
         {
-            _dataService = dataService;
+            _scopeFactory = scopeFactory;
         }
         public string ReturnData()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("--> Getting data from No SQL Databse...");
-            _dataService.GetProductData("https://ckziumm.edu.pl");
-            Console.ResetColor();
+            //_dataService.GetProductData("https://ckziumm.edu.pl");
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dataService = scope.ServiceProvider.GetRequiredService<IDataService>();
+                dataService.GetProductData("https://ckziumm.edu.pl");
+                Console.ResetColor();
 
-            return("No SQL Data from DB");
+                return("No SQL Data from DB");
+            }
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using DiApi.DataServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IDataRepo,NoSqlDataRepo>();
+builder.Services.AddSingleton<IDataRepo,NoSqlDataRepo>();
 builder.Services.AddScoped<IDataService, HttpDataService>();
 
 var app = builder.Build();
